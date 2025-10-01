422334e5d64f9b10a42a2d75479eb790b08b1d89
Ana Carolina Machado <anacarolinamachado366@gmail.com>
2025-09-30 21:52:42 -0300

    Criação dos retornos de cardapio e mesa controller

---
+++ Comanda.Api/Comanda.Api/Controllers/CardapioItemController.cs
@@ -0,0 +1,74 @@
+﻿using Comanda.Api.Models;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Comanda.Api.Controllers
+{
+    //CRIA A ROTA DO COTROLADOR
+    [Route("api/[controller]")]
+    [ApiController] // DEFINE QUE ESSA CLASSE É UM CONTROLADOR DE API
+    public class CardapioItemController : ControllerBase // HERDA DE CONTROLLERBASE para PODER RESPONDER A REQUISICOES HTTP
+    {
+        List<CardapioItem> cardapios = new List<CardapioItem>()
+        {
+            new CardapioItem
+            {
+                Id = 1,
+                Descricao = "Coxinha de frango com catupiry",
+                Preco = 5.50m,
+                PossuiPreparo = true,
+            },
+            new CardapioItem
+            {
+                Id = 2,
+                Descricao = "Pastel de carne com queijo",
+                Preco = 7.00m,
+                PossuiPreparo = true,
+            }
+        };
+
+        // METODO GET que retorna a lista de cardapio
+        // GET: api/<CardapioItemController>
+        [HttpGet] // ANOTACAO QUE INDICA QUE ESSE METODO RESPONDE A REQUISICOES GET
+        public IResult GetCardapio()
+        {
+            // CRIA UMA LISTA ESTATICA DE CARDAPIO E TRANSFORMA EM JSON
+            return Results.Ok(cardapios);
+        }
+
+
+        // GET api/<CardapioItemController>/1
+        [HttpGet("{id}")]
+        public IResult Get(int id)
+        {
+            // BUSCAR NA LISTA de cardapios de acorod com o Id do parametro
+            //joga o valor para a variavel o primeiro elemento de acordo com o id
+            var cardapio = cardapios.FirstOrDefault(c => c.Id == id);
+            if (cardapio is null)
+            {
+                return Results.NotFound("Cardápio não encontrado!");
+            }
+            // retorna o valor para o ednpoint da api
+            return Results.Ok(cardapio);
+        }
+
+        // POST api/<CardapioItemController>
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/<CardapioItemController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/<CardapioItemController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ Comanda.Api/Comanda.Api/Models/Mesa.cs
@@ -0,0 +1,15 @@
+﻿namespace Comanda.Api.Models
+{
+    public class Mesa
+    {
+        public int Id { get; set; }
+        public int NumeroMesa { get; set; }
+        public int SituacaoMesa { get; set; }
+    }
+    public enum SituacaoMesa
+    {
+        Livre = 0,
+        Ocupada = 1,
+        Reservado = 3
+    }
+}
